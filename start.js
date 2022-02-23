cd208122fd083978897e4a0ec02b6a89ec609b28
Amber Nifong <amber.nifong@gmail.com>
2022-02-23 16:57:45 -0500

    amber made db files can't sync db though

--- start.js
+++ start.js
@@ -1,10 +1,11 @@
-const db = require('./db/database')
-const app = require('../server/index');
+const db = require('./Avatar/server/db')
+const app = require('./Avatar/server/index');
 
 const port = process.env.PORT || 3000;
 
 
 db.sync()  // sync our database
   .then(function(){
+    console.log('db synced')
     app.listen(port) // then start listening with our express server once we have synced
   })
